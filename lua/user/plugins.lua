b7e1bd8e1b4ab79bc1628e3022d17a98dac4cf7c
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-07-28 17:12:02 -0300

    chore: install devicons

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -4,6 +4,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim"
   use "nvim-lua/plenary.nvim"
   use "nvim-lua/popup.nvim"
+  use "kyazdani42/nvim-web-devicons"
   
   use { "nvim-telescope/telescope.nvim", tag = '0.1.x' }
   use "kyazdani42/nvim-tree.lua"
