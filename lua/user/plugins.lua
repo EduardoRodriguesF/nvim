01fae9ca7672dae209442a499b269c637269b3ed
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-07-28 16:56:46 -0300

    add plugins file

---
+++ lua/user/plugins.lua
@@ -0,0 +1,9 @@
+local packer = require('packer')
+
+return packer.startup(function(use)
+  use "wbthomason/packer.nvim"
+  use "nvim-lua/plenary.nvim"
+  use "nvim-lua/popup.nvim"
+  
+  use { "nvim-telescope/telescope.nvim", tag = '0.1.x' }
+end)
