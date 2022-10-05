30d0440faf0fda2f71b92f179cd8ffade362b66a
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-10-05 19:50:28 -0300

    feat: add barbar plugin for better tabs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -28,6 +28,7 @@ return packer.startup(function(use)
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use "kyazdani42/nvim-tree.lua"
   use "nvim-lualine/lualine.nvim"
+  use "romgrk/barbar.nvim"
 
   use "getomni/neovim"
 end)
