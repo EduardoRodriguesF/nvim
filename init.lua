7b194624385ea467ec3ab92aa89951b2cda2dd86
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-03-08 20:39:22 -0300

    add special keymaps for rust_analyzer

--- init.lua
+++ init.lua
@@ -63,7 +63,16 @@ require("mason-lspconfig").setup_handlers {
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
   ["rust_analyzer"] = function()
-    require("rust-tools").setup {}
+    local rt = require("rust-tools")
+
+    rt.setup {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+          vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+      }
+    }
   end
 }
 
