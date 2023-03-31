688cc07bdc41d67f846328ba1e9a5351470873bb
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-03-31 19:58:34 -0300

    Add treesitter context plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -14,6 +14,7 @@ return packer.startup(function(use)
   use "lewis6991/gitsigns.nvim"
 
   use "nvim-treesitter/nvim-treesitter"
+  use 'nvim-treesitter/nvim-treesitter-context'
   use 'williamboman/mason.nvim'
   use "jose-elias-alvarez/null-ls.nvim"
   use 'ron-rs/ron.vim'
