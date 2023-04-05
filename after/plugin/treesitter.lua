6c7c037f535fd15c51fd223cbf0d5d6098f1f67c
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-04-05 15:54:26 -0300

    Move treesitter migration to proper file

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+require "user.treesitter.migration" -- Hopefully temporary
+
 require("nvim-treesitter.configs").setup({
   ensure_installed = { "lua", "javascript" },
   sync_install = false,
--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
 require "user.options"
 require "user.keymaps"
-
--- Hopefully temporary
-require "user.treesitter.migration"
