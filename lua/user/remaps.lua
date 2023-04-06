46c8d647f38c478e4563f7f928954793e6d34163
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-04-06 19:46:06 -0300

    Remove keymaps alias

--- lua/user/remaps.lua
+++ lua/user/remaps.lua
@@ -1,44 +1,38 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
-
--- Alias for set keymap api
-local keymap = vim.api.nvim_set_keymap
-
-keymap("", "<space>", "<nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = "<space>"
+vim.g.maplocalleader = "<space>"
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<c-h>", "<c-w>h", opts)
-keymap("n", "<c-j>", "<c-w>j", opts)
-keymap("n", "<c-k>", "<c-w>k", opts)
-keymap("n", "<c-l>", "<c-w>l", opts)
+vim.keymap.set("n", "<c-h>", "<c-w>h", opts)
+vim.keymap.set("n", "<c-j>", "<c-w>j", opts)
+vim.keymap.set("n", "<c-k>", "<c-w>k", opts)
+vim.keymap.set("n", "<c-l>", "<c-w>l", opts)
 
 -- Tabs
-keymap("n", "<A->>", ":tabnext<cr>", opts)
-keymap("n", "<A-<>", ":tabprevious<cr>", opts)
+vim.keymap.set("n", "<A->>", ":tabnext<cr>", opts)
+vim.keymap.set("n", "<A-<>", ":tabprevious<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<c-up>", ":resize -2<cr>", opts)
-keymap("n", "<c-down>", ":resize +2<cr>", opts)
-keymap("n", "<c-left>", ":vertical resize -2<cr>", opts)
-keymap("n", "<c-right>", ":vertical resize +2<cr>", opts)
+vim.keymap.set("n", "<c-up>", ":resize -2<cr>", opts)
+vim.keymap.set("n", "<c-down>", ":resize +2<cr>", opts)
+vim.keymap.set("n", "<c-left>", ":vertical resize -2<cr>", opts)
+vim.keymap.set("n", "<c-right>", ":vertical resize +2<cr>", opts)
 
 -- Insert --
 -- Quickly exit mode
-keymap("i", "jk", "<esc>", opts)
-keymap("i", "kj", "<esc>", opts)
+vim.keymap.set("i", "jk", "<esc>", opts)
+vim.keymap.set("i", "kj", "<esc>", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<a-j>", ":m .+1<cr>==", opts)
-keymap("v", "<a-k>", ":m .-2<cr>==", opts)
+vim.keymap.set("v", "<a-j>", ":m .+1<cr>==", opts)
+vim.keymap.set("v", "<a-k>", ":m .-2<cr>==", opts)
 
 -- Paste without losing register
-keymap("v", "p", '"_dP', opts)
+vim.keymap.set("v", "p", '"_dP', opts)
