2ddb08ca3feecf846ebcee6a4851d4186726c410
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-04-07 19:56:21 -0300

    [telescope] Remove extra keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,8 +8,6 @@ require('telescope').setup({
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {}) -- Project files
 vim.keymap.set("n", "<C-p>", builtin.git_files, {}) -- Git files
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 vim.keymap.set("n", "<leader>ps", function ()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
