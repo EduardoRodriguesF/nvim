06a18a23e24b1a7f1369a38c138381aeded081ce
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-04-07 23:05:53 -0300

    Add clipboard system yank

--- lua/user/remaps.lua
+++ lua/user/remaps.lua
@@ -3,7 +3,6 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Normal --
 -- Better window navigation
 vim.keymap.set("n", "<c-h>", "<c-w>h", opts)
 vim.keymap.set("n", "<c-j>", "<c-w>j", opts)
@@ -16,6 +15,10 @@ vim.keymap.set("n", "<c-down>", ":resize +2<cr>", opts)
 vim.keymap.set("n", "<c-left>", ":vertical resize -2<cr>", opts)
 vim.keymap.set("n", "<c-right>", ":vertical resize +2<cr>", opts)
 
--- Visual --
 -- Paste without losing register
-vim.keymap.set("v", "p", '"_dP', opts)
+vim.keymap.set("x", "p", '"_dP', opts)
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
