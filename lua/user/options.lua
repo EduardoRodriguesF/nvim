9e3f5899ab04927d311a2f10c2978e52737f60f6
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-04-07 23:40:00 -0300

    Add strategic spacings between options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,22 +2,31 @@ local opt = vim.opt
 
 opt.guicursor = ""
 
+opt.mouse = "c"
+
 opt.hlsearch = false
 opt.incsearch = true
 
 opt.backup = false
+
 opt.expandtab = true
 opt.shiftwidth = 2
 opt.tabstop = 2
+
 opt.number = true
 opt.relativenumber = true
+opt.cursorline = true
+opt.wrap = false
+
 opt.scrolloff = 8
 opt.sidescrolloff = 8
+
 opt.updatetime = 300
+
 opt.fixendofline = true
+
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldenable = false
-opt.cursorline = true
-opt.wrap = false
+
 opt.wildignore = "*.o,*.a,*.so,*.pyc,*.swp,.git,.git/*,*.class,*/target/*,.idea/*,.vscode/*,node_modules,node_modules/*"
